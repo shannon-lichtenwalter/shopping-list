589f87eb0c2955088bc3a758386e65d87b7d2b01
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-19 14:27:09 -0600

    can add items

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict';
 
 function addItem() {
-  
+  $('form').submit(function () {
+    event.preventDefault();
+    console.log($('input').val());
+    let newItem = $('input').val();
+    // I want to make it a list item and add the same classes
+    $('ul').append(`
+    <li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+    $('input').val('');
+  });
 }
 
-$(addItem)
+$(addItem);
